a9a42b05d920c0b90a5aecb101637c3a32959126
Nathan Myers <nathan.myers@steelseries.com>
2019-04-01 09:15:39 -0500

    adding files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'src/index.html',
+    })
+  ],
 }
